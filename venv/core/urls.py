df1f2b0cb75702e99be9893912a9971ea59175ba
educintrabr <educin15@hotmail.com>
2019-01-14 12:44:22 -0200

    4º Alteração/ Meu Notebook

---
+++ venv/core/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url, include
+from .views import (
+    home, pessoas, veiculos, 
+    movRotativo, mensalista, movMensalista, 
+    nova_pessoa, novo_veiculo, novo_movRotativo,
+    novo_mensalista, novo_movMensalista, update_pessoa,
+    update_veiculo, update_mensalista, update_movRotativo,
+    update_movMensalista)
+
+urlpatterns = [
+    url(r'^$', home, name='core_home'),
+    url(r'^pessoas/$', pessoas, name='core_listaPessoas'),
+    url(r'^nova_pessoa/$', nova_pessoa, name='core_novaPessoa'),
+    url(r'^update_pessoa/(?P<id>\d+)/$', update_pessoa, name='core_updatePessoa'),
+
+    url(r'^veiculos/$', veiculos, name='core_listaVeiculos'),
+    url(r'^update_veiculo/(?P<id>\d+)/$', update_veiculo, name='core_updateVeiculo'),
+    url(r'^novo_veiculo/$', novo_veiculo, name='core_novoVeiculo'),
+    
+    url(r'^movRotativo/$', movRotativo , name='core_listaMovRotativo'),
+    url(r'^update_movRotativo/(?P<id>\d+)/$', update_movRotativo, name='core_updateMovRotativo'),
+    url(r'^novo_movRotativo/$', novo_movRotativo , name='core_novoMovRotativo'),
+
+    
+    url(r'^mensalista/$', mensalista , name='core_listaMensalista'),
+    url(r'^update_mensalista/(?P<id>\d+)/$', update_mensalista, name='core_updateMensalista'),
+    url(r'^novo_mensalista/$', novo_mensalista , name='core_novoMensalista'),
+    
+    url(r'^movMensalista/$', movMensalista , name='core_listaMovMensalista'),
+    url(r'^update_movMensalista/(?P<id>\d+)/$', update_movMensalista, name='core_movMensalista'),
+    url(r'^novo_movMensalista/$', novo_movMensalista , name='core_novoMovMensalista')
+]
